2a03a57b638f59f8b48037162c1c3b239b5fa48d
Paul Mandal <paul.mandal@gmail.com>
2020-10-01 12:06:38 -0600

    Make map icon swap drawables every 2 seconds

--- app/src/main/java/com/paulmandal/atak/plugin/example/PluginMarkerIconWidget.java
+++ app/src/main/java/com/paulmandal/atak/plugin/example/PluginMarkerIconWidget.java
@@ -14,11 +14,15 @@ import com.atakmap.android.widgets.RootLayoutWidget;
 import com.atakmap.coremap.maps.assets.Icon;
 
 public class PluginMarkerIconWidget extends MarkerIconWidget implements MapWidget.OnClickListener {
+    private static final int DELAY_BETWEEN_ICON_UPDATES_MS = 2000;
+
     private final static int ICON_WIDTH = 32;
     private final static int ICON_HEIGHT = 32;
 
     private PluginDropDownReceiver pluginDropDownReceiver;
     private MapView mapView;
+
+    private boolean keepAlive = true;
 
     public PluginMarkerIconWidget(MapView mapView, PluginDropDownReceiver pluginDropDownReceiver) {
         this.pluginDropDownReceiver = pluginDropDownReceiver;
@@ -31,16 +35,9 @@ public class PluginMarkerIconWidget extends MarkerIconWidget implements MapWidge
         LinearLayoutWidget brLayout = root.getLayout(RootLayoutWidget.BOTTOM_RIGHT);
         brLayout.addWidget(this);
 
-        String imageUri = "android.resource://com.paulmandal.atak.plugin.example/" + R.drawable.ic_map_overlay;
+        updateIcon(false);
 
-        Icon.Builder builder = new Icon.Builder();
-        builder.setAnchor(0, 0);
-        builder.setColor(Icon.STATE_DEFAULT, Color.WHITE);
-        builder.setSize(ICON_WIDTH, ICON_HEIGHT);
-        builder.setImageUri(Icon.STATE_DEFAULT, imageUri);
-
-        Icon icon = builder.build();
-        setIcon(icon);
+        new Thread(this::updateIconLoop).start();
     }
 
     @Override
@@ -60,5 +57,41 @@ public class PluginMarkerIconWidget extends MarkerIconWidget implements MapWidge
         RootLayoutWidget root = (RootLayoutWidget) mapView.getComponentExtra("rootLayoutWidget");
         LinearLayoutWidget brLayout = root.getLayout(RootLayoutWidget.BOTTOM_RIGHT);
         brLayout.removeWidget(this);
+
+        this.keepAlive = false;
+    }
+
+    private void updateIconLoop() {
+        boolean iconState = false;
+
+        while (keepAlive) {
+            iconState = !iconState;
+
+            updateIcon(iconState);
+
+            try {
+                Thread.sleep(DELAY_BETWEEN_ICON_UPDATES_MS);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void updateIcon(boolean split) {
+        int drawableId = R.drawable.ic_map_overlay;
+        if (split) {
+            drawableId = R.drawable.ic_map_overlay_split;
+        }
+
+        String imageUri = "android.resource://com.paulmandal.atak.plugin.example/" + drawableId;
+
+        Icon.Builder builder = new Icon.Builder();
+        builder.setAnchor(0, 0);
+        builder.setColor(Icon.STATE_DEFAULT, Color.WHITE);
+        builder.setSize(ICON_WIDTH, ICON_HEIGHT);
+        builder.setImageUri(Icon.STATE_DEFAULT, imageUri);
+
+        Icon icon = builder.build();
+        setIcon(icon);
     }
 }
